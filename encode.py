886b4f12e08e84ed02b41f8fb2c0050afef4c88b
FlintVSgamin <149016434+FlintVSgamin@users.noreply.github.com>
2023-10-25 20:05:01 -0400

    Delete encode.py to clean up repository

--- encode.py
+++
@@ -1,9 +0,0 @@
-#Aaron Townsend
-def encode(original_pass):
-    result = ''
-    for i in range(len(original_pass)):
-        num = original_pass[i]
-        result += chr((ord(num) + 3))
-    return result
-
-
